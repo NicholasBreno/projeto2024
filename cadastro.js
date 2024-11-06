d8b6d8707522fb28ac7e801eb1b5769dd1bac47d
Paulo Vítor <paulovitoralsp@yahoo.com.br>
2024-11-06 13:58:56 -0300

    Adicionada e estilizada página para edição de perfil do usuário.

--- cadastro.js
+++ cadastro.js
@@ -19,3 +19,38 @@ if (!emailPattern.test(email)) {
 alert("Cadastro realizado com sucesso!!!")
 
 }
+
+//Retirar para integração ao back end.
+
+/*fetch('https://api.seusite.com/cadastro', {  // Substitua pelo URL da sua API de cadastro
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            fullName: fullName,
+            email: email,
+            password: password,
+            birthdate: birthdate
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            // Supondo que o backend retorne um sucesso
+            alert('Cadastro realizado com sucesso!');
+            // Redireciona para a página de login ou dashboard, conforme a lógica do seu sistema
+            window.location.href = 'login.html';  // Ou outra página após o cadastro
+        } else {
+            alert('Erro ao cadastrar. Tente novamente.');
+        }
+    })
+    .catch(error => {
+        console.error('Erro no cadastro:', error);
+        alert('Erro ao tentar se cadastrar. Tente novamente mais tarde.');
+    });
+    
+
+    // Fim da integração com o backend
+}
+*/
